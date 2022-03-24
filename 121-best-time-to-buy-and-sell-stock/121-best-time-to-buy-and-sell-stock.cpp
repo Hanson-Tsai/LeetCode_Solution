2be8cfdcee832d87962d95ea7151d5f148314ff9
Hanson-Tsai <82587276+Hanson-Tsai@users.noreply.github.com>
2022-03-25 01:48:31 +0800

    Time: 235 ms (18.95%), Space: 93.3 MB (53.62%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    //Greedy O(n)
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() < 2) return 0;      
+        int maxProfit = 0, minPrice = prices[0];
+    
+        for(int i = 1; i < prices.size(); i++) {
+            if(prices[i] > prices[i - 1]) {
+                maxProfit = max(maxProfit, prices[i] - minPrice);       
+            } else {
+                minPrice = min(minPrice, prices[i]);
+            }
+        }   
+
+        return maxProfit;
+    }
+    /* DP Max Subarray
+    int maxProfit(int[] prices) {
+        int maxCur = 0, maxSoFar = 0;
+        for(int i = 1; i < prices.size(); i++) {
+            maxCur = max(0, maxCur += prices[i] - prices[i-1]);
+            maxSoFar = max(maxCur, maxSoFar);
+        }
+        return maxSoFar;
+    }
+    */
+};
