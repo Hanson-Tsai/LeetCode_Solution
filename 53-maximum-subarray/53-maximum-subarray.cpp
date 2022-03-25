267012fe68956e7ff5a0deaa9e1a60bfc08d09a4
Hanson-Tsai <82587276+Hanson-Tsai@users.noreply.github.com>
2022-03-25 14:40:48 +0800

    Time: 128 ms (71.93%), Space: 67.9 MB (12.01%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    // DP O(n)
+    int maxSubArray(vector<int>& nums) {
+        int sum = 0, smax = INT_MIN;
+        for (int num : nums) {
+            sum += num;
+            smax = max(smax, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return smax;
+    }
+};
