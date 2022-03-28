e97a23687d4789c2e69e38c55263acc964723d23
Hanson-Tsai <82587276+Hanson-Tsai@users.noreply.github.com>
2022-03-29 03:42:30 +0800

    Time: 22 ms (69.11%), Space: 19.5 MB (98.15%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if(nums.size() <= 1)return nums[0];
+        int bound = nums.size()/2;
+        int count = 0;
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != nums[i+1]){
+                count = 0;
+            }else{
+                count++;
+            }
+            
+            if(count >= bound)return nums[i];
+        }
+        
+        return nums[nums.size()];
+        
+    }
+};
