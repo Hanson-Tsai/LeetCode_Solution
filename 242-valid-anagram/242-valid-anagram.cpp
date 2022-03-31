e222a1b87f42842f9403fa3aeb1e490c28fc27c0
Hanson-Tsai <82587276+Hanson-Tsai@users.noreply.github.com>
2022-04-01 00:43:18 +0800

    Time: 8 ms (80.48%), Space: 7.1 MB (99.48%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s==t)return true;
         if(s.size() != t.size())return false;
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
         
-        for(int i=0; i<s.size();i++){
-            if(s[i] != t[i])return false;
+        int hash[26] = {};
+        for(int i=0; i<s.size(); i++){
+            hash[s[i]-'a']++;
+            hash[t[i]-'a']--;
         }
+        
+        for(int i=0; i<26;i++){
+            if(hash[i] != 0)return false;
+        }
+        
         
         return true;
     }
