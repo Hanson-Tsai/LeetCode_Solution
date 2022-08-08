99165cc1fcff5d1a5cd72027e5e5f1d762231479
Hanson-Tsai <jerry10127@gmail.com>
2022-08-08 17:15:15 +0800

    Time: 71 ms (27.41%), Space: 27.6 MB (12.40%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -8,7 +8,7 @@ private:
     int bs(vector<int>& nums, int target, int left, int right){
         if(left > right)return -1;
         
-        int mid = left + (right - left) / 2;
+        int mid = (right + left) / 2;
         
         if(nums[mid] == target)return mid;
         else if(nums[mid] > target)return bs(nums, target, left, mid-1);
